7e554584017741fde117902aaf7373dc72ce6d96
Danielle Matton <danimatton@gmail.com>
2019-11-04 03:21:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,7 +35,7 @@ end
 
 def apply_clearance(cart)
   cart.each do |product_name, stats|
-   stats[:price] -= stats[:price] * 0.2
+   stats[:price] -= stats[:price] * 0.2 if stats[:clearance]
 end
 cart
 end
